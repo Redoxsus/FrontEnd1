074f9771973e1d90eced23a45047d9c9ef84628c
DarahBeku <bryan.535230012@stu.untar.ac.id>
2024-09-02 17:01:42 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+
+document.getElementById('Linkedin-click').onclick = function() {
+    window.open('https://id.linkedin.com/', '_blank');
+}
+
+document.getElementById('instagram-click').onclick = function() {
+    window.open('https://www.instagram.com/', '_blank');
+}
+
+document.getElementById('Facebook-click').onclick = function() {
+    window.open('https://www.facebook.com/', '_blank');
+}
